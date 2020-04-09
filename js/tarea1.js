e5a90d644137f7e1480ae103a96e5f833aedf24f
edilbertoashleyvalentina <ashleyvalenet261205@gmail.com>
2020-04-09 14:46:33 -0500

    agrego archivo tarea1.js

---
+++ js/tarea1.js
@@ -0,0 +1,24 @@
+function numero1(valor) {
+	alert(valor);
+	//var numero = document.getElementById('numero').value;
+switch(valor) {
+	case "1":
+	alert ("has escrito el numero uno");
+	break;
+	case "2":
+	alert ("has escrito el numero dos");
+	break;
+	case "3":
+	alert ("has escrito el numero tres");
+	break;
+	case "4":
+	alert ("has escrito el numero cuatro");
+	break;
+	case "5":
+	alert ("has escrito el numero cinco");
+	break;
+	default:
+	alert ("Lo siento esa opción no es valida");
+}
+return;
+}
