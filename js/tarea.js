538ffdc94f98650ff52e9dab4937874c29b6782e
ashley <ashleyvalenet261205@gmail.com>
2020-04-09 13:05:43 -0500

    tarea1

---
+++ js/tarea.js
@@ -0,0 +1,22 @@
+function numero1() {
+var numero = document.getElementById('numero').value;
+	if (numero==1) {
+		alert ("has escrito el numero uno");
+	}else if (numero==2) {
+		alert ("has escrito el numero dos");
+	}else if (numero==3){
+		alert ("has escrito el numero tres");
+	}else if (numero==4) {
+		alert ("has escrito el numero cuatro");
+	}else if (numero==5) {
+		alert ("has escrito el numero cinco");
+	}else{
+		alert ("Lo siento esa opción no es valida");
+	}
+
+	// if (numero==1) {
+	// 	alert ("has escrito el numero uno");
+	// }else if{
+
+	// }
+}
---
+++ tarea1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>tarea</title>
+	<script type="text/javascript" src="js/tarea.js"></script>
+</head>
+<body>
+<form> digite numeros de 1 al 5<br>
+	<input type="text" name="numero" id= "numero" onblur="numero1()">
+</form>
+<div id="respuesta" name="respuesta"></div>
+</body>
+</html>
